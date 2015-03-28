7a3ea85eea911fa8c46180376e99b4c3b8cb5c16
Tim C <foamyguy@gmail.com>
2015-03-27 21:05:00 -0500

    updated some things

--- js/init.js
+++ js/init.js
@@ -12,7 +12,7 @@
 
 		// skelJS (probably don't need to change anything here unless you know what you're doing)
 			skelJS: {
-				prefix: 'static/css/style',
+				prefix: 'css/style',
 				resetCSS: true,
 				boxModel: 'border',
 				useOrientation: true,
